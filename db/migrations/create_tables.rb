6536dd35f25a8472df17066a46df8b5d2fb9ca71
Kim Hart <kimhart90@gmail.com>
2015-12-07 10:38:56 -0500

    create_tables file

---
+++ db/migrations/create_tables.rb
@@ -0,0 +1,7 @@
+require_relative "../db_config"
+require_relative "articles_categories"
+require_relative "create_articles"
+require_relative "create_categories"
+require_relative "create_users"
+
+
