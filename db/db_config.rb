a1f6d6d2fdd468d63027262fa451ef25bd1b0d88
Kim Hart <kimhart90@gmail.com>
2015-12-07 10:19:33 -0500

    fixed db config

--- db/db_config.rb
+++ db/db_config.rb
@@ -1,5 +1,18 @@
 require "active_record"
 
+if ENV['RACK_ENV'] == "production"
+  require 'uri'
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection({
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  })
+else
+
 ActiveRecord::Base.logger = Logger.new(STDERR)
 
 ActiveRecord::Base.establish_connection(
@@ -10,3 +23,4 @@ ActiveRecord::Base.establish_connection(
 Dir.glob("models/*.rb").each do |path|
   require_relative "../#{path}"
 end 
+end
